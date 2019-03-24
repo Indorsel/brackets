31b3cdd9032d1f517f33efc99d8400e19af03292
Tsimur Mirzayeu <timoha993@gmail.com>
2019-03-24 16:58:02 +0300

    feat: create bad solution 13/7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var count = 0;
+  var array = [];
+  var newStr = str;
+  for (var i = 0; i < newStr.length; i++) {
+    if (newStr[i] == '(') {
+      array.push(newStr[i]);
+      count = i;
+    } else if (newStr[i] == ')' && newStr[count] == '(') {
+      array.pop();
+      count--;
+    } else if (newStr[i] == '[') {
+      array.push(newStr[i]);
+      count = i;
+    } else if (newStr[i] == ']' && newStr[count] == '[') {
+      array.pop();
+      count--;
+    } else if (newStr[i] == '{') {
+      array.push(newStr[i]);
+      count = i;
+    } else if (newStr[i] == '}' && newStr[count] == '{') {
+      array.pop();
+      count--;
+    } else if (newStr[i] == '|') {
+      array.push(newStr[i]);
+      count = i;
+    } else if (newStr[i] == '|' && newStr[count] == '|') {
+      array.pop();
+      count--;
+    }
+  }
+
+  if (array.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
